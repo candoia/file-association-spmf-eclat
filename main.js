27185b4a975736287b2280564604a08e0ae63c96
Nitin Mukesh Tiwari <nmtiwari@iastate.edu>
2016-08-21 22:34:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+$('#content').show();
+  let json = api.boa.run('revisions.boa','arff');
+  console.log("boa finished");
+  var result = api.spmf.eclat(json, '0.01');
+$('#loading').hide();
+$('#content').show();
+var lines = result.split('\n');
+for(var line of lines){
+  line += '<br />';
+  $(document.body).append(line);
+}
+$('#content').hide();
